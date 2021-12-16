4a689d38e9f9d6f4308767b593a13f445f1b6fc1
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2021-12-16 10:58:49 +0500

    initial commit

---
+++ app/src/main/java/com/example/databindingdemo/Student.kt
@@ -0,0 +1,12 @@
+package com.example.databindingdemo
+
+/**
+ * Created by Sadaqat Panhwer
+ * https://panhwersadaqat.github.io/
+ * on 12/13/21.
+ */
+data class Student(
+    var id:Int,
+    var name:String,
+    var email:String
+)
